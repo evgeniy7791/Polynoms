42c844e2d661dc84c8689f17b0b7a8e4f6b8c28b
evgeniy7791 <nikefc7906@gmail.com>
2023-04-28 11:27:18 +0300

    New ordered table and hash table 2(open adr)

--- containers/open_adress.h
+++ containers/open_adress.h
@@ -1,1 +1,107 @@
-﻿
+﻿#ifndef OPEN_ADRESS_H
+#define OPEN_ADRESS_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+#include <iostream>
+#include <vector>
+#include <functional>
+
+template<typename Key, typename Value>
+class HashTable2 {
+private:
+    std::vector<std::pair<Key, Value>> table;
+    int size;
+
+    size_t hash(const Key& key) const {
+        return std::hash<Key>{}(key) % table.size();
+    }
+
+    void rehash() {
+        // перехэширование
+        int newSize = table.size() * 2;
+        std::vector<std::pair<Key, Value>> newTable(newSize);
+        for (auto& p : table) {
+            int i = hash(p.first);
+            while (newTable[i].first != Key{} && newTable[i].first != p.first) {
+                i = (i + 1) % newSize;
+            }
+            newTable[i] = p;
+        }
+        table = std::move(newTable);
+    }
+
+public:
+    HashTable2() : size(0), table(8) {}
+
+    void insert2(const Key& key, const Value& value) {
+        if (size >= table.size() * 0.7) {
+            rehash();
+        }
+        int i = hash(key);
+        while (table[i].first != Key{} && table[i].first != key) {
+            i = (i + 1) % table.size();
+        }
+        table[i] = { key, value };
+        size++;
+    }
+
+    void insert(const Key& key, const Value& value) {
+        insert2(key, value);
+    }
+
+    bool find(const Key& key, Value& value) const {
+        int i = hash(key);
+        while (table[i].first != Key{}) {
+            if (table[i].first == key) {
+                value = table[i].second;
+                return true;
+            }
+            i = (i + 1) % table.size();
+        }
+        return false;
+    }
+
+    void remove(const Key& key) {
+        int i = hash(key);
+        while (table[i].first != Key{}) {
+            if (table[i].first == key) {
+                table[i].first = Key{};
+                size--;
+                return;
+            }
+            i = (i + 1) % table.size();
+        }
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    Value& operator[](const Key& key) {
+        int i = hash(key);
+        while (table[i].first != Key{}) {
+            if (table[i].first == key) {
+                return table[i].second;
+            }
+            i = (i + 1) % table.size();
+        }
+        insert(key, Value{});
+        return table[hash(key)].second;
+    }
+
+    void print() const {
+        for (const auto& p : table) {
+            if (p.first != Key{}) {
+                std::cout << "Key: " << p.first << ", Value: " << p.second << std::endl;
+            }
+        }
+    }
+};
+
+
+
+#endif
--- containers/ordered_table.h
+++ containers/ordered_table.h
@@ -1,1 +1,86 @@
-#pragma once
+#ifndef ORDERED_TABLE_H
+#define ORDERED_TABLE_H
+
+#include <vector>
+
+template<typename Key, typename Value>
+class OrderedTable {
+private:
+    std::vector< std::pair<Key, Value>> Otable;
+public:
+    OrderedTable() = default;
+
+    OrderedTable(const OrderedTable& t) = default;
+    OrderedTable& operator= (const OrderedTable& t) = default;
+
+    ~OrderedTable() = default;
+
+    Value* find(const Key& key) {
+        for (auto& p : Otable) {
+            if (p.first == key) {
+                return &p.second;
+            }
+        }
+        return nullptr;
+    }
+
+    void remove(Key key) {
+        auto it = Otable.begin();
+        while (it != Otable.end()) {
+            if (it->first == key) {
+                it = Otable.erase(it);
+                return;
+            }
+            else {
+                ++it;
+            }
+        }
+    }
+
+
+
+    void insert(const std::pair<Key, Value> & value) {
+        auto it = Otable.begin();
+        for (; it != Otable.end(); ++it) {
+            if (it->first == value.first) {
+                it->second = value.second;
+                return;
+            }
+            else if (it->first > value.first) {
+                break;
+            }
+        }
+        Otable.insert(it, std::pair<Key, Value>(value.first, value.second));
+    }
+   
+    void insert(Key key, Value val) {
+        size_t i = 0;
+        while (i < Otable.size() && Otable[i].first < key) {
+            ++i;
+        }
+        Otable.insert(Otable.begin() + i, std::make_pair(key, val));
+    }
+
+    std::size_t size() const {
+        return Otable.size();
+    }
+
+    Value& operator[](const Key& key) {
+        for (auto& p : Otable) {
+            if (p.first == key) {
+                return p.second;
+            }
+        }
+        Otable.push_back(std::pair<Key, Value> (key, Value()));
+        return Otable.back().second;
+    }
+
+    void print() const {
+        for (const auto& elem : Otable) {
+            std::cout << elem.first << ", " << elem.second << std::endl;
+        }
+    }
+};
+
+
+#endif
